07a82b0496c293ea984260ae41700ea11d24fd5f
NatC02 <NatanCeballos02@gmail.com>
2021-09-27 05:35:21 -0400

    Code and style fo top-level layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,34 @@ import scrollTo from 'gatsby-plugin-smoothscroll'
 class Header extends React.Component {
   render() {
     return (
-      <section className="blue">
-        <h1>
-          Hi, I'm<strong> Natan Ceballos</strong>.
+      <section className="blue sectionHeader">
+        <h1 >
+          Hi, I'm <strong className="animate__bounceIn" > Natan Ceballos.</strong>
         </h1>
-        <p>I'm a full stack developer.</p>
+        <h2>
+          I'm a <strong className="animate__bounceIn">Frontend </strong>
+          Software Engineer.
+        </h2>
 
-        <ul>
-          <button onClick={() => scrollTo('#one')}>Skills</button>
-          <button onClick={() => scrollTo('#two')}>Projects</button>
-          <button onClick={() => scrollTo('#three')}>Blog</button>
-          <button onClick={() => scrollTo('#four')}>About</button>
-          <button onClick={() => scrollTo('#five')}>Contact</button>
-          <button onClick={() => scrollTo('#six')}>Socials</button>
+        <ul className="animate__bounceIn">
+          <button onClick={() => scrollTo('#one')}>
+            <h4>Skills</h4>
+          </button>
+          <button onClick={() => scrollTo('#two')}>
+            <h4>Projects</h4>
+          </button>
+          {/* <button onClick={() => scrollTo('#three')}>
+            <h4>Blog</h4>
+          </button> */}
+          <button onClick={() => scrollTo('#four')}>
+            <h4>About</h4>
+          </button>
+          <button onClick={() => scrollTo('#five')}>
+            <h4>Contact</h4>
+          </button>
+          {/* <button onClick={() => scrollTo('#six')}>
+            <h4>Socials</h4>
+          </button> */}
         </ul>
 
         <div className="curve"></div>
