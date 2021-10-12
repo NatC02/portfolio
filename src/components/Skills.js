652c144c524f6b5efc0c4f065231eba4085a1e93
NatC02 <NatanCeballos02@gmail.com>
2021-10-12 13:47:53 -0400

    Added onViewport Animations for Skills section fixed layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 import HTML5 from '../assets/icons/HTML5.svg'
 import CSS3 from '../assets/icons/CSS3.svg'
-import Bootstrap from '../assets/icons/Bootstrap.svg'
-import JS from '../assets/icons/JavaScript.svg'
+// import Bootstrap from '../assets/icons/Bootstrap.svg'
+import Handlebars from '../assets/icons/Handlebars.svg'
+import Javascript from '../assets/icons/JavaScript.svg'
+import Gatsby from '../assets/icons/Gatsby.svg'
 import ReactJS from '../assets/icons/React.svg'
 
-import VSCode from '../assets/icons/VisualStudio.svg'
 import Git from '../assets/icons/Git.svg'
+import DigitalOcean from '../assets/icons/DigitalOcean.svg'
+import Postman from '../assets/icons/Postman.svg'
+import Netlify from '../assets/icons/Netlify.svg'
 import Webpack from '../assets/icons/Webpack.svg'
 import Jira from '../assets/icons/JiraSoftware.svg'
 
 import NodeJS from '../assets/icons/Node.svg'
 import Express from '../assets/icons/Expressjs.svg'
 import RESTfulAPI from '../assets/icons/API.svg'
+import MongoDB from '../assets/icons/Mongodb.svg'
 
 class Skills extends React.Component {
   render() {
@@ -32,138 +38,211 @@ class Skills extends React.Component {
             ></path>
           </svg>
         </div>
-        <h1>
-          Skills
-        </h1>
-
-        <section class="section skills" id="skills">
-          <div class="section-center skills-center">
-
-            <article>
-              <h3>Front End</h3>
-              <div class="skill-box-2 flexCenter">
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={HTML5}
-                    loading="lazy"
-                    alt="HTML5"
-                  />
-                  <p class="skill-name">HTML5</p>
-                </div>
-
-                <div>
-                  <img class="skill-icn" src={CSS3} loading="lazy" alt="CSS3" />
-                  <p class="skill-name">CSS3</p>
-                </div>
-
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={Bootstrap}
-                    loading="lazy"
-                    alt="Bootstrap"
-                  />
-                  <p class="skill-name">Bootsrap</p>
-                </div>
-
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={JS}
-                    loading="lazy"
-                    alt="javascript"
-                  />
-                  <p class="skill-name">Javascript</p>
-                </div>
-
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={ReactJS}
-                    loading="lazy"
-                    alt="React"
-                  />
-                  <p class="skill-name">React</p>
-                </div>
+        <h1>Skills</h1>
+        <div class="containerTech">
+          <ScrollAnimation
+            animateIn="animate__fadeInLeft"
+            offset={75}
+            animateOnce={true}
+            duration={0.6}
+          >
+            {/* animate__animated animate__faster animate__fadeInLeft */}
+            <div class="groupOne ">
+              <div class="gOneTitle">Frontend</div>
+              <div class="oone">
+                <img
+                  className="skill-icn"
+                  src={Javascript}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">Javascript</p>
               </div>
-            </article>
-            
-            <article>
-              <h3>Tools</h3>
-              <div class="skill-box-2">
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={VSCode}
-                    loading="lazy"
-                    alt="vscode"
-                  />
-                  <p class="skill-name">VS Code</p>
-                </div>
-
-                <div>
-                  <img class="skill-icn" src={Git} loading="lazy" alt="Git" />
-                  <p class="skill-name">Git</p>
-                </div>
-
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={Webpack}
-                    loading="lazy"
-                    alt="Webpack"
-                  />
-                  <p class="skill-name">Webpack</p>
-                </div>
-
-                <div>
-                  <img class="skill-icn" src={Jira} loading="lazy" alt="Jira" />
-                  <p class="skill-name">Jira</p>
-                </div>
+              <div class="otwo">
+                <img
+                  className="skill-icn"
+                  src={ReactJS}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">React</p>
               </div>
-            </article>
-
-            <article>
-              <h3>Back End</h3>
-              <div class="skill-box-1">
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={NodeJS}
-                    loading="lazy"
-                    alt="Node"
-                  />
-                  <p class="skill-name">Node</p>
-                </div>
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={Express}
-                    loading="lazy"
-                    alt="Express"
-                  />
-                  <p class="skill-name">Express</p>
-                </div>
-                <div>
-                  <img
-                    class="skill-icn"
-                    src={RESTfulAPI}
-                    loading="lazy"
-                    alt="RESTfulAPI"
-                  />
-                  <p class="skill-name">RESTfulAPI</p>
-                </div>
+              <div class="othree">
+                <img
+                  className="skill-icn"
+                  src={Gatsby}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">Gatsby</p>
               </div>
-            </article>
+              <div class="ofour">
+                <img
+                  className="skill-icn"
+                  src={Handlebars}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">Handlebars</p>
+              </div>
+              <div class="ofive">
+                <img
+                  className="skill-icn"
+                  src={HTML5}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">HTML5</p>
+              </div>
+              <div class="osix">
+                <img
+                  className="skill-icn"
+                  src={CSS3}
+                  loading="lazy"
+                  alt="Frontend"
+                />
+                <p class="skill-name">CSS3</p>
+              </div>
+            </div>
+          </ScrollAnimation>
 
+          {/*
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           */}
 
+          <ScrollAnimation
+         animateIn="animate__fadeInLeft"
+         offset={75}
+         animateOnce={true}
+         duration={0.6}
+          >
+            <div class="groupTwo">
+              <div class="gTwoTitle">Tools</div>
+              <div class="ooone">
+                <img
+                  className="skill-icn"
+                  src={Git}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Git</p>
+              </div>
+              <div class="ootwo">
+                <img
+                  className="skill-icn"
+                  src={Postman}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Postman</p>
+              </div>
+              <div class="oothree">
+                <img
+                  className="skill-icn"
+                  src={Jira}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Jira</p>
+              </div>
+              <div class="oofour">
+                <img
+                  className="skill-icn"
+                  src={Webpack}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Webpack</p>
+              </div>
+              <div class="oofive">
+                <img
+                  className="skill-icn"
+                  src={Netlify}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Netlify</p>
+              </div>
+              <div class="oosix">
+                <img
+                  className="skill-icn"
+                  src={DigitalOcean}
+                  loading="lazy"
+                  alt="Tools"
+                />
+                <p class="skill-name">Digital Ocean</p>
+              </div>
+            </div>
+          </ScrollAnimation>
 
+          {/*
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           *
+           */}
 
+          <ScrollAnimation
+           animateIn="animate__fadeInLeft"
+           offset={75}
+           animateOnce={true}
+           duration={0.6}
+          >
+            <div class="groupThree">
+              <div class="gThreeTitle">Backend</div>
 
-
-          </div>
-        </section>
+              <div class="oooOne">
+                <img
+                  className="skill-icn"
+                  src={NodeJS}
+                  loading="lazy"
+                  alt="Backend"
+                />
+                <p class="skill-name">Node</p>
+              </div>
+              <div class="oooTwo">
+                <img
+                  className="skill-icn"
+                  src={RESTfulAPI}
+                  loading="lazy"
+                  alt="Backend"
+                />
+                <p class="skill-name">RESTful API</p>
+              </div>
+              <div class="oooThree">
+                <img
+                  className="skill-icn"
+                  src={MongoDB}
+                  loading="lazy"
+                  alt="Backend"
+                />
+                <p class="skill-name">MongoDb</p>
+              </div>
+              <div class="oooFour">
+                <img
+                  className="skill-icn"
+                  src={Express}
+                  loading="lazy"
+                  alt="Backend"
+                />
+                <p class="skill-name">Express</p>
+              </div>
+            </div>
+          </ScrollAnimation>
+        </div>
       </section>
     )
   }
