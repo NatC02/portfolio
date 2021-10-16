c94b86965ca22c54f753754de2cd30d0ceb23eb7
NatC02 <NatanCeballos02@gmail.com>
2021-10-16 17:36:22 -0400

    fixed Contact Form sunmission w/ Netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ScrollAnimation from 'react-animate-on-scroll'
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 class Contact extends React.Component {
   render() {
@@ -7,79 +7,63 @@ class Contact extends React.Component {
       <section id="five" className="sectionContact">
         <h1>Contact</h1>
         <ScrollAnimation
-              animateIn="animate__fadeInUp"
-              offset={75}
-              animateOnce={true}
-              duration={0.7}
-            >
-        <form
-          name="contact-form"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          id="contactForm"
-          onsubmit="return validate()"
-          action="contact.php"
-          method="post"
+          animateIn="animate__fadeInUp"
+          offset={75}
+          animateOnce={true}
+          duration={0.7}
         >
-          <input
-            class="formInput"
-            type="text"
-            id="name"
-            name="name"
-            autocorrect="off"
-            placeholder="*Name"
-          />
-          <input
-            class="formInput"
-            type="email"
-            name="email"
-            id="email"
-            autocapitalize="off"
-            autocorrect="off"
-            placeholder="*Email"
-          />
-          <textarea
-            class="formInput"
-            name="message"
-            id="message"
-            placeholder="*Message"
-          ></textarea>
-
-          <br />
-          <input class="submitForm" type="reset" value="Clear your message" />
-          <input class="submitForm" type="submit" value="Send" />
-        </form>
-
-        <footer>
-          <div class="sm-handle">
-            <button>
-              <a
-                href="https://www.linkedin.com/in/natan-ceballos-66b1a6187"
-                class="sm-button"
-              >
-                <i class="fab fa-linkedin"> LinkedIn </i>
-              </a>
-            </button>
-            <button>
-              <a href="https://github.com/NatC02/" class="sm-button">
-                <i class="fab fa-github"> GitHub</i>
-              </a>
-            </button>
-          </div>
-
-          <p class="copyright">
-            Copyright 2021 |
-            <a href="https://github.com/NatC02/"> ❤️ Natan Ceballos </a>
-            <br />
-            *Form Not Working*
-          </p>
-        </footer>
+          <form
+            name="contactform"
+            method="post"
+            data-netlify="true"
+            onSubmit="submit"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contactform" />
+            <div hidden>
+              <input name="bot-field" />
+            </div>
+            <div>
+              <label>
+                Name <br /> <input type="text" name="name" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="email">Email</label> <br />
+              <input id="email" type="email" name="email" />
+            </div>
+            <div>
+              <label>
+                Message: <br />
+                <textarea id="message" name="message"></textarea>
+              </label>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+          <footer>
+            <div class="sm-handle">
+              <button>
+                <a
+                  href="https://www.linkedin.com/in/natan-ceballos-66b1a6187"
+                  class="sm-button"
+                >
+                  <i class="fab fa-linkedin"> LinkedIn </i>
+                </a>
+              </button>
+              <button>
+                <a href="https://github.com/NatC02/" class="sm-button">
+                  <i class="fab fa-github"> GitHub</i>
+                </a>
+              </button>
+            </div>
+            <p class="copyright">
+              Copyright 2021 |
+              <a href="https://github.com/NatC02/"> ❤️ Natan Ceballos </a>
+            </p>
+          </footer>
         </ScrollAnimation>
-
       </section>
-    )
+    );
   }
 }
-
-export default Contact
+export default Contact;
