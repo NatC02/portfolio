1ca9d5cb7f931bcb4de1cb3bb8f886aa5f1fb5e6
NatC02 <NatanCeballos02@gmail.com>
2021-10-18 21:13:07 -0400

    gatsby config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/', // This path is relative to the root of the site.
+        icon: 'src/assets/imagesReadMe/fullscreenScreenshot.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-smoothscroll',
