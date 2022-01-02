7c8c16844e8b50e2089925912d70c0e5403adea9
NatC02 <NatanCeballos02@gmail.com>
2022-01-01 21:19:45 -0500

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#73abe3",
+        green: "#78c828",
+        tan: "#edbf85",
+        pink: "#e373ab",
+        dark: "#282c35",
+      },
+    },
+  },
+  plugins: [],
+}
