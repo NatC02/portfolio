1a302bf377b2c5bddc56476cc24e60a7dc947eac
NatC02 <NatanCeballos02@gmail.com>
2022-01-01 21:23:44 -0500

    css styles applied everywhere

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+// Tailwind
+import "./src/styles/global.css"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// Portfolio
+import "./src/index.scss"
+// custom CSS styles
+import "./src/style.css"
